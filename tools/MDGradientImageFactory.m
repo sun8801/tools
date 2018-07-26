e521ec72fc562192ab33f607021c032020c62786
sun <sun8801@users.noreply.github.com>
2018-07-26 15:33:31 +0800

    支持颜色组

--- tools/MDGradientImageFactory.m
+++ tools/MDGradientImageFactory.m
@@ -30,14 +30,23 @@
                               startPoint:(CGPoint)startPoint
                                 endPoint:(CGPoint)endPoint
                                     size:(CGSize)size {
+    return [self gradientImageWithGradientColors:@[startColor, endColor]
+                                      startPoint:startPoint
+                                        endPoint:endPoint
+                                            size:size];
+}
+
++ (UIImage *)gradientImageWithGradientColors:(NSArray <UIColor *>*)gradientColors
+                                  startPoint:(CGPoint)startPoint
+                                    endPoint:(CGPoint)endPoint
+                                        size:(CGSize)size {
     if (CGSizeEqualToSize(size, CGSizeZero)) {
         size = CGSizeMake(20, 10);
     }
-    CAGradientLayer *gradientLayer = [self gradientLayerWithStartColor:startColor
-                                                              endColor:endColor
-                                                            startPoint:startPoint
-                                                              endPoint:endPoint
-                                                                  size:size];
+    CAGradientLayer *gradientLayer = [self gradientLayerWithGradientColors:gradientColors
+                                                                startPoint:startPoint
+                                                                  endPoint:endPoint
+                                                                      size:size];
     
     UIGraphicsBeginImageContextWithOptions(size, YES, 0);
     CGContextRef contextRef = UIGraphicsGetCurrentContext();
@@ -74,8 +83,22 @@
                                       startPoint:(CGPoint)startPoint
                                         endPoint:(CGPoint)endPoint
                                             size:(CGSize)size {
+    return [self gradientLayerWithGradientColors:@[startColor, endColor]
+                                      startPoint:startPoint
+                                        endPoint:endPoint
+                                            size:size];
+}
+
++ (CAGradientLayer *)gradientLayerWithGradientColors:(NSArray <UIColor *> *)gradientColors
+                                          startPoint:(CGPoint)startPoint
+                                            endPoint:(CGPoint)endPoint
+                                                size:(CGSize)size {
+    NSMutableArray *colors = [NSMutableArray arrayWithCapacity:gradientColors.count];
+    [gradientColors enumerateObjectsUsingBlock:^(UIColor * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        [colors addObject:((__bridge id)obj.CGColor)];
+    }];
     CAGradientLayer *gradientLayer = [CAGradientLayer layer];
-    gradientLayer.colors = @[(__bridge id)startColor.CGColor, (__bridge id)endColor.CGColor];
+    gradientLayer.colors = colors;
     gradientLayer.startPoint = startPoint;
     gradientLayer.endPoint = endPoint;
     gradientLayer.frame = CGRectMake(0, 0, size.width, size.height);
